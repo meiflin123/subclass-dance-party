0ff699afee85498a4fe30d91e9475c661b615006
meiflin123 <meiflin123@gmail.com>
2018-06-18 19:10:42 -0700

    Refactored code to pseudoclassical style and passed all tests

--- src/init.js
+++ src/init.js
@@ -19,12 +19,13 @@ $(document).ready(function() {
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
+  
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+    // do we need to add the 'new' keyword here?
+    var dancer = new dancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
